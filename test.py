c6c5db35fe160318570787a1049c5fc6449277d0
savvas <savvasswe@gmail.com>
2023-10-03 11:12:13 +0200

    split pickles into chunks

--- test.py
+++ test.py
@@ -5,9 +5,10 @@ import sys
 import csv
 from threading import Thread
 import pickle
+import numpy as np
 
 pickle_path = "data/"
-pickle_name = "temp_pickle.pkl"
+pickle_name = "temp_pickle"
 raw_data_path = "data/raw_test/"
 
 
@@ -26,6 +27,8 @@ __is_loading = False
 __loaded_chunk = pd.DataFrame()
 __chunk_size = 2000
 
+__n_pkl_files = 10
+
 def __prefetch(first_elem_time):
     #Prefetch data, once done isLoading = false
     __isLoading = False
@@ -40,7 +43,7 @@ def fetch_preload():
     None
 
 def is_ready():
-    return __is_loading
+    return not __is_loading
 
 def test_pkl():
     df = pd.read_pickle(join(pickle_path,pickle_name))
@@ -53,9 +56,22 @@ def __merge_and_pickle_csvs():
     #Merge all data from txt, tsv and csv's into a list
     for input_file in sorted(input_files):
         li.append(pd.read_csv(input_file, header=None, sep='\t', dtype=__DTYPES))
-    #Make list into a dataframe and save as pickle
+    #Make list into a dataframe
     df = pd.concat(li, axis=0, ignore_index=True)
-    df.to_pickle(join(pickle_path,pickle_name))
+    #Add column headers and sort by time
+    df.columns = __DTYPES.keys()
+    df.sort_values(by="time")
+    #Split dataframe into n_chunks and pickle each chunk
+    chunks = np.array_split(df, __n_pkl_files)
+    i = 0
+    for chunk in chunks:
+        df_ch = pd.DataFrame(chunk)
+        df_ch.to_pickle(join(pickle_path, "%s%s.pkl" % (pickle_name,str(i))))
+        i = i + 1
+        
+
+    #Convert and save dataframe to pickle
+   # df.to_pickle(join(pickle_path,pickle_name,".pkl"))
 
 #Used only for creation of database, do not call normally
 def __prep_db():
@@ -63,4 +79,11 @@ def __prep_db():
     #load into pickle file
     None
 
-test_pkl()
+def __del_db():
+    for file in os.listdir(pickle_path):
+        if os.fsdecode(file).endswith(".pkl"):
+            os.remove(join(pickle_path, file))
+
+__del_db()
+__merge_and_pickle_csvs()
+#test_pkl()
