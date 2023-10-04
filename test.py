fc2a682f45f2f05571bad91c4ef7e5cbc559b3cf
savvas <savvasswe@gmail.com>
2023-10-04 12:20:47 +0200

    fetch added

--- test.py
+++ test.py
@@ -1,23 +1,37 @@
-import pandas as pd
-import os
+from threading import Thread
 from os.path import join
+from constants import *
+import pandas as pd
+import numpy as np
+import os
 import sys
 import csv
-from threading import Thread
 import pickle
-import numpy as np
-from constants import *
 
 __is_loading = False
 __loaded_chunk = pd.DataFrame()
+__loading_chunk = pd.DataFrame()
 
-def __prefetch(first_elem_time):
-    #Prefetch data, once done isLoading = false
-    __isLoading = False
+def fetch_chunk(from_time, to_time):
+    '''Returns a dataframe with headers, containing rows from from_time to to_time'''
+    #Return None if input is faulty
+    if to_time < from_time or from_time < 0 or to_time < 0:
+        return None
+    #Get all pickles in directory
+    pickles = [f for f in os.listdir(Paths.PICKLE_DIR) if f.endswith(".pkl")]
+    to_load = []
+    for p in pickles:
+        times = p.replace(".pkl",'').split("_", 1)
+        #Check if file contains data in range
+        if(int(times[1]) >= from_time and int(times[0]) <= to_time):
+            #Append to return dataframe
+            to_load.append(pd.read_pickle(join(Paths.PICKLE_DIR, p)))
+            print("Range contained in pickle: %s" % (p))
+    return pd.concat(to_load)
 
-def preload_chunk(first_elem_time):
+def preload_chunk(from_time, to_time):
     __isLoading = True
-    prefetch_thread = Thread(target = __prefetch, args = (first_elem_time, ))
+    prefetch_thread = Thread(target = __prefetch, args = (from_time, to_time, ))
     prefetch_thread.start()
 
 def fetch_preload():
@@ -25,10 +39,14 @@ def fetch_preload():
     None
 
 def is_ready():
-    '''Returns true if preloaded chunk is ready for fetching'''
+    '''Returns true if chunk is ready for fetching'''
     return not __is_loading
 
-def __ferment_pickles():
+def __prefetch(from_time, to_time):
+    #Prefetch data, once done isLoading = false
+    __isLoading = False
+
+def __make_pickles():
     '''Create pickle files for each raw data file, name = firstTimeStamp_lastTimeStamp.pkl'''
     #For all data files ending with .txt, .tsv or .csv, reformat them into pickles, adding corresponding headers in the process
     input_files = [join(Paths.RAW_DIR, f) for f in os.listdir(Paths.RAW_DIR) if f.endswith(".txt") or f.endswith(".tsv") or f.endswith(".csv")]
@@ -37,6 +55,7 @@ def __ferment_pickles():
         #Fetches keys for dtypes, to use as names for headers
         df.columns = TableData.DTYPES.keys()
         df.to_pickle(join(Paths.PICKLE_DIR, "%s_%s.pkl" % (df[Keys.TIME_INTERVAL].iloc[1],df[Keys.TIME_INTERVAL].iloc[-1])))
+        print("Successfully pickled %s" % (input_file))
 
 def __del_db():
     '''Deletes all pickles from pickles directory'''
@@ -47,7 +66,10 @@ def __del_db():
 def __prep_db():
     '''Recreates database from scratch using all raw .txt files'''
     __del_db()
-    __ferment_pickles()
+    __make_pickles()
 
 
-__prep_db()
+#__prep_db()
+#print(is_ready())
+print(fetch_chunk(1383346000000, 1383346600000))
+
