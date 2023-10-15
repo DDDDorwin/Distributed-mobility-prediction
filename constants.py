e9ec0f09280ddf6f0d1840e382c6fd71e8b88d8b
Salum Nassor <salim.nassor.sn@gmail.com>
2023-10-16 01:47:14 +0200

    Work contributed by others, just a linting that will make the project consistent with formatting

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-'''
+"""
 This module is to define constants, mainly string keys. Code should be clear
 from all magic string keys and register them instead here. The classes are
 there to distinguish between topics and hold the constants as class variables.
@@ -19,43 +19,43 @@ from constant import Keys
 etc. Then use them in your code as e.g.
 
 Keys.SQUARE_ID
-'''
+"""
 
 
 from os.path import join as pjoin
 
 
 class Paths(object):
-    '''File and directory paths on uppmax'''
+    """File and directory paths on uppmax"""
 
-    DATA_DIR = pjoin('proj', 'uppmax2023-2-33', 'nobackup', 'data')
-    RAW_DIR = pjoin(DATA_DIR, 'raw')
-    PREPROCESSED_DIR = pjoin(DATA_DIR, 'preprocessed')
-    GROUPED_CC_DIR = pjoin(PREPROCESSED_DIR, 'grouped_country_codes')
-    MERGED_TO_SIZE_DIR = pjoin(PREPROCESSED_DIR, 'merged_to_size')
+    DATA_DIR = pjoin("proj", "uppmax2023-2-33", "nobackup", "data")
+    RAW_DIR = pjoin(DATA_DIR, "raw")
+    PREPROCESSED_DIR = pjoin(DATA_DIR, "preprocessed")
+    GROUPED_CC_DIR = pjoin(PREPROCESSED_DIR, "grouped_country_codes")
+    MERGED_TO_SIZE_DIR = pjoin(PREPROCESSED_DIR, "merged_to_size")
 
 
 class Keys(object):
-    '''Column names for dataframes'''
+    """Column names for dataframes"""
 
     # raw data set
-    SQUARE_ID = 'square_id'
-    TIME_INTERVAL = 'time_interval'
-    COUNTRY_CODE = 'country_code'
-    SMS_IN = 'sms_in'
-    SMS_OUT = 'sms_out'
-    CALL_IN = 'call_in'
-    CALL_OUT = 'call_out'
-    INTERNET = 'internet'
+    SQUARE_ID = "square_id"
+    TIME_INTERVAL = "time_interval"
+    COUNTRY_CODE = "country_code"
+    SMS_IN = "sms_in"
+    SMS_OUT = "sms_out"
+    CALL_IN = "call_in"
+    CALL_OUT = "call_out"
+    INTERNET = "internet"
 
 
 class TableData(object):
-    '''Numpy/Pandas datatypes and other descriptors for columns in dataframes
-    
+    """Numpy/Pandas datatypes and other descriptors for columns in dataframes
+
     Values should be string names of pandas dtypes
     https://pandas.pydata.org/pandas-docs/stable/user_guide/basics.html#basics-dtypes
-    '''
-    
+    """
+
     # index of each column in the raw data set
     INDICES = {
         Keys.SQUARE_ID: 0,
@@ -68,14 +68,14 @@ class TableData(object):
         Keys.INTERNET: 7,
     }
 
-    # raw data set 
+    # raw data set
     DTYPES = {
-        Keys.SQUARE_ID: 'int16',
-        Keys.TIME_INTERVAL: 'int64',
-        Keys.COUNTRY_CODE: 'int8',
-        Keys.SMS_IN: 'float64',
-        Keys.SMS_OUT: 'float64',
-        Keys.CALL_IN: 'float64',
-        Keys.CALL_OUT: 'float64',
-        Keys.INTERNET: 'float64',
+        Keys.SQUARE_ID: "int16",
+        Keys.TIME_INTERVAL: "int64",
+        Keys.COUNTRY_CODE: "int8",
+        Keys.SMS_IN: "float64",
+        Keys.SMS_OUT: "float64",
+        Keys.CALL_IN: "float64",
+        Keys.CALL_OUT: "float64",
+        Keys.INTERNET: "float64",
     }
