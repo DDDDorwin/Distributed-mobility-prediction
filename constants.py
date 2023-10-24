f6f18a11bbf1fb480dbebb12c570fb529eebe33d
savvas <savvasswe@gmail.com>
2023-10-24 09:59:06 +0200

    gitignore

--- constants.py
+++ constants.py
@@ -29,7 +29,8 @@ class Paths(object):
     '''File and directory paths on uppmax'''
 
     # directories
-    DATA_DIR = pjoin('/', 'proj', 'uppmax2023-2-33', 'nobackup', 'data')
+    #DATA_DIR = pjoin('/', 'proj', 'uppmax2023-2-33', 'nobackup', 'data')
+    DATA_DIR = pjoin('./data')
     RAW_DIR = pjoin(DATA_DIR, 'raw')
     PICKLE_DIR = pjoin(DATA_DIR, 'pickles')
     PREPROCESSED_DIR = pjoin(DATA_DIR, 'preprocessed')
