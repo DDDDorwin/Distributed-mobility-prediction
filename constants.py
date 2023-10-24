d1e16c824eb0b8c4fb515220077a0ff4c7c2792b
savvas <savvasswe@gmail.com>
2023-10-24 10:00:50 +0200

    constants

--- constants.py
+++ constants.py
@@ -29,8 +29,7 @@ class Paths(object):
     '''File and directory paths on uppmax'''
 
     # directories
-    #DATA_DIR = pjoin('/', 'proj', 'uppmax2023-2-33', 'nobackup', 'data')
-    DATA_DIR = pjoin('./data')
+    DATA_DIR = pjoin('/', 'proj', 'uppmax2023-2-33', 'nobackup', 'data')
     RAW_DIR = pjoin(DATA_DIR, 'raw')
     PICKLE_DIR = pjoin(DATA_DIR, 'pickles')
     PREPROCESSED_DIR = pjoin(DATA_DIR, 'preprocessed')
