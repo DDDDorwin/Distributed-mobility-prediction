cba7567e1fef574d898f17e50d9f6bc1de281cb3
Mithzyl <517711766@qq.com>
2023-11-29 19:54:28 +0100

    implemented early stopping

--- utils/wandb_plot.py
+++ utils/wandb_plot.py
@@ -8,6 +8,7 @@ def plot_test_data(args, pred):
     pred = torch.cat(pred).numpy().reshape(-1, 1)
     df = pd.DataFrame(pred)
     df[Keys.INDEX] = [i for i in range(len(pred))]
+    df.columns = [Keys.INTERNET, Keys.INDEX]
     # columns = [Keys.INTERNET, Keys.INDEX]
     # test_table = wandb.Table(data=pred)
 
