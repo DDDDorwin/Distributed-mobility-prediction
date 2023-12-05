a663f637cb31513bc5456b0f3283e32cfbae4172
Mithzyl <517711766@qq.com>
2023-12-05 16:28:37 +0100

    implemented early stopping

---
+++ utils/temp_pickle.py
@@ -0,0 +1,44 @@
+import os
+import pandas as pd
+from data.data import Dataset
+from utils.util import pickle_normalization
+from typing import TypeVar
+from constants import Paths, Keys, TableData
+from os.path import join
+
+def make_general_pickle():
+    size = 0
+    input_file = r"D:\project\python\project_cs\data\raw_merged.tsv"
+    df = pd.read_csv(input_file, header=None, sep="\t", names=TableData.DTYPES.keys(), dtype=TableData.DTYPES)
+    length = len(df)
+    df['start_time'] = pd.to_datetime(df[Keys.TIME_INTERVAL], unit='ms', utc=True).dt.tz_convert(
+        'CET').dt.tz_localize(None)
+    # df = df.fillna(0)
+    df = df.groupby([pd.Grouper(key='start_time', freq='10Min')]).sum()
+    # df = df.groupby([Keys.SQUARE_ID, pd.Grouper(key='start_time', freq='10Min')]).agg()
+    df = df.drop(Keys.TIME_INTERVAL, axis=1)
+    df = df.drop(Keys.COUNTRY_CODE, axis=1)
+    # ids.name = Keys.SQUARE_ID
+    # ids.index = [i for i in range(len(ids))]
+    normalized = pickle_normalization(df)
+    # normalized[Keys.INDEX] = ids
+    length = len(normalized)
+    # Add corresponding indexes to rows
+    normalized[Keys.INDEX] = [i for i in range(size, size + length)]
+
+    # normalized = pd.concat([ids, normalized], axis=1)
+    # normalized.insert(5, Keys.SQUARE_ID, ids)
+    normalized = normalized.set_index(Keys.INDEX)
+    # normalized[Keys.SQUARE_ID] = ids
+    # normalized = pd.concat([ids, normalized], axis=1)
+    # Fetches keys for dtypes, to use as names for headers
+    normalized.columns = TableData.NORMALIZED_DTYPES.keys()
+    # Make pickles with name: startIndex_endIndex.pkl
+    normalized.to_pickle(r"D:\project\python\project_cs\data\mean.pkl")
+    # Increment size
+    size += length
+    print("Successfully pickled %s" % (input_file))
+
+
+if __name__=='__main__':
+    make_general_pickle()
