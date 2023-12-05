f9d40636ce729f75b962afb005b61d44ce50043d
Alexis <517711766@qq.com>
2023-12-05 15:57:53 +0100

    implemented embedding

--- utils/wandb_plot.py
+++ utils/wandb_plot.py
@@ -4,11 +4,16 @@ from constants import Keys
 import pandas as pd
 from utils.util import inverse_normalization, normalization, load_sum_data
 
-def plot_test_data(args, pred):
+def plot_test_data(args, pred, test_loader):
+    true_data = []
+    for x, y in test_loader:
+        true_data.append(y)
+    true_data = torch.cat(true_data).numpy().reshape(-1, 1)
     pred = torch.cat(pred).numpy().reshape(-1, 1)
     df = pd.DataFrame(pred)
+    df['ground_truth'] = true_data
     df[Keys.INDEX] = [i for i in range(len(pred))]
-    df.columns = [Keys.INTERNET, Keys.INDEX]
+    df.columns = ['prediction', 'ground_truth', Keys.INDEX]
     # columns = [Keys.INTERNET, Keys.INDEX]
     # test_table = wandb.Table(data=pred)
 
