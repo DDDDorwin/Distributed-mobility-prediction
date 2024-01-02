f053db0f0092274d86b6f3a98778ad93244c846f
Salum Nassor <salim.nassor.sn@gmail.com>
2024-01-02 22:56:24 +0100

    fix: mse visualization test

--- tests/mse_visualization_test.py
+++ tests/mse_visualization_test.py
@@ -14,13 +14,13 @@ class TestMSEPlot(unittest.TestCase):
         mse_values_model2 = [0.2, 0.18, 0.15, 0.13, 0.11]
         epochs_model2 = [1, 2, 3, 4, 5]
         msevis.create_mse_json(
-            os.path.join(self.rootPath, "assets/jsons/evaluation_metrics/mse/model2.json"),
+            "assets/jsons/evaluation_metrics/mse/model2.json",
             mse_values_model2,
             epochs_model2,
         )
         msevis.plot_mse_from_json(
-            os.path.join(self.rootPath, "assets/jsons/evaluation_metrics/mse/"),
-            os.path.join(self.rootPath, "assets/images/evaluation_metrics/mse.png"),
+            "assets/jsons/evaluation_metrics/mse/",
+            "assets/images/evaluation_metrics/mse.png",
         )
 
     def test_more_models(self):
@@ -29,30 +29,30 @@ class TestMSEPlot(unittest.TestCase):
         epochs_model1 = [1, 2, 3, 4, 5]
         epochs_model2 = [1, 2, 3, 4, 5]
         msevis.create_mse_json(
-            os.path.join(self.rootPath, "assets/jsons/evaluation_metrics/mse/model1.json"),
+            "assets/jsons/evaluation_metrics/mse/model1.json",
             mse_values_model1,
             epochs_model1,
         )
         msevis.create_mse_json(
-            os.path.join(self.rootPath, "assets/jsons/evaluation_metrics/mse/model2.json"),
+            "assets/jsons/evaluation_metrics/mse/model2.json",
             mse_values_model2,
             epochs_model2,
         )
         msevis.plot_mse_from_json(
-            os.path.join(self.rootPath, "assets/jsons/evaluation_metrics/mse/"),
-            os.path.join(self.rootPath, "assets/images/evaluation_metrics/mse.png"),
+            "assets/jsons/evaluation_metrics/mse/",
+            "assets/images/evaluation_metrics/mse.png",
         )
 
     def test_no_jsons(self):
-        msevis.plot_mse_from_json(os.path.join(self.rootPath, "assets/jsons/evaluation_metrics/mse/"))
+        msevis.plot_mse_from_json("assets/jsons/evaluation_metrics/mse/", "assets/images/evaluation_metrics/mse.png")
 
     def tearDown(self):
-        if os.path.exists("../assets/jsons/evaluation_metrics/mse/model1.json"):
-            os.remove("../assets/jsons/evaluation_metrics/mse/model1.json")
-        if os.path.exists("../assets/jsons/evaluation_metrics/mse/model2.json"):
-            os.remove("../assets/jsons/evaluation_metrics/mse/model2.json")
-        if os.path.exists("../assets/images/evaluation_metrics/mse.png"):
-            os.remove("../assets/images/evaluation_metrics/mse.png")
+        if os.path.exists("assets/jsons/evaluation_metrics/mse/model1.json"):
+            os.remove("assets/jsons/evaluation_metrics/mse/model1.json")
+        if os.path.exists("assets/jsons/evaluation_metrics/mse/model2.json"):
+            os.remove("assets/jsons/evaluation_metrics/mse/model2.json")
+        if os.path.exists("assets/images/evaluation_metrics/mse.png"):
+            os.remove("assets/images/evaluation_metrics/mse.png")
 
 
 if __name__ == "__main__":
